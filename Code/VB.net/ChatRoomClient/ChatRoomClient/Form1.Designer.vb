9a197f7d1b7bbd011bb7405be1eb15d32a54b09a
iberro <iberro@msn.com>
2016-03-13 23:47:09 +0200

    add .net project

---
+++ Code/VB.net/ChatRoomClient/ChatRoomClient/Form1.Designer.vb
@@ -0,0 +1,229 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ButtonConnect = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.TextUsername = New System.Windows.Forms.TextBox()
+        Me.TextPort = New System.Windows.Forms.TextBox()
+        Me.TextIP = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextMessages = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.TextChat = New System.Windows.Forms.TextBox()
+        Me.ButtonSend = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ButtonConnect
+        '
+        Me.ButtonConnect.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ButtonConnect.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonConnect.Location = New System.Drawing.Point(256, 58)
+        Me.ButtonConnect.Name = "ButtonConnect"
+        Me.ButtonConnect.Size = New System.Drawing.Size(157, 23)
+        Me.ButtonConnect.TabIndex = 0
+        Me.ButtonConnect.Text = "Connect"
+        Me.ButtonConnect.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.Controls.Add(Me.TextUsername)
+        Me.GroupBox1.Controls.Add(Me.TextPort)
+        Me.GroupBox1.Controls.Add(Me.ButtonConnect)
+        Me.GroupBox1.Controls.Add(Me.TextIP)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 181)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(427, 94)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Connection"
+        '
+        'TextUsername
+        '
+        Me.TextUsername.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextUsername.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextUsername.Location = New System.Drawing.Point(117, 55)
+        Me.TextUsername.Name = "TextUsername"
+        Me.TextUsername.Size = New System.Drawing.Size(129, 26)
+        Me.TextUsername.TabIndex = 5
+        '
+        'TextPort
+        '
+        Me.TextPort.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextPort.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextPort.Location = New System.Drawing.Point(304, 23)
+        Me.TextPort.Name = "TextPort"
+        Me.TextPort.Size = New System.Drawing.Size(109, 26)
+        Me.TextPort.TabIndex = 4
+        Me.TextPort.Text = "12345"
+        '
+        'TextIP
+        '
+        Me.TextIP.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextIP.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextIP.Location = New System.Drawing.Point(117, 23)
+        Me.TextIP.Name = "TextIP"
+        Me.TextIP.Size = New System.Drawing.Size(129, 26)
+        Me.TextIP.TabIndex = 3
+        Me.TextIP.Text = "192.168.1.1"
+        '
+        'Label3
+        '
+        Me.Label3.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(252, 26)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Port :"
+        '
+        'Label2
+        '
+        Me.Label2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(7, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Usename :"
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(7, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(112, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Host address :"
+        '
+        'TextMessages
+        '
+        Me.TextMessages.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextMessages.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextMessages.Location = New System.Drawing.Point(12, 12)
+        Me.TextMessages.Multiline = True
+        Me.TextMessages.Name = "TextMessages"
+        Me.TextMessages.Size = New System.Drawing.Size(427, 163)
+        Me.TextMessages.TabIndex = 0
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox2.Controls.Add(Me.ButtonSend)
+        Me.GroupBox2.Controls.Add(Me.TextChat)
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(13, 282)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(426, 62)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Chat"
+        '
+        'TextChat
+        '
+        Me.TextChat.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextChat.Enabled = False
+        Me.TextChat.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextChat.Location = New System.Drawing.Point(10, 25)
+        Me.TextChat.Name = "TextChat"
+        Me.TextChat.Size = New System.Drawing.Size(321, 26)
+        Me.TextChat.TabIndex = 0
+        '
+        'ButtonSend
+        '
+        Me.ButtonSend.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ButtonSend.Enabled = False
+        Me.ButtonSend.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonSend.Location = New System.Drawing.Point(337, 25)
+        Me.ButtonSend.Name = "ButtonSend"
+        Me.ButtonSend.Size = New System.Drawing.Size(75, 26)
+        Me.ButtonSend.TabIndex = 1
+        Me.ButtonSend.Text = "Send"
+        Me.ButtonSend.UseVisualStyleBackColor = True
+        '
+        'Form
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(448, 352)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.TextMessages)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form"
+        Me.Text = "Chat Room"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ButtonConnect As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TextUsername As TextBox
+    Friend WithEvents TextPort As TextBox
+    Friend WithEvents TextIP As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextMessages As TextBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents ButtonSend As Button
+    Friend WithEvents TextChat As TextBox
+End Class
---
+++ Code/VB.net/ChatRoomClient/ChatRoomClient/Form1.vb
@@ -0,0 +1,95 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+Imports System.Threading
+Class Form
+
+    Dim port As String
+    Dim ip As String
+    Dim username As String
+    Dim socket As Socket
+    Private thread As Thread
+    Dim messages As String = ""
+
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextPort.TextChanged
+
+    End Sub
+
+    Private Sub ButtonConnect_Click(sender As Object, e As EventArgs) Handles ButtonConnect.Click
+        If ButtonConnect.Text = "Connect" Then
+            port = TextPort.Text
+            ip = TextIP.Text
+            username = TextUsername.Text
+
+            Try
+                Dim bytes(255) As Byte
+                socket = New Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+                socket.Connect(New IPEndPoint(IPAddress.Parse(ip), port))
+
+                Dim bytesCount As Integer = socket.Receive(bytes)
+                If String.Compare(System.Text.Encoding.ASCII.GetString(bytes), "Username :") Then
+                    socket.Send(System.Text.Encoding.ASCII.GetBytes(username & vbCrLf))
+                    thread = New Thread(AddressOf Receive)
+                    thread.IsBackground = True
+                    thread.Start()
+                Else
+                    MsgBox("Error")
+                    Return
+                End If
+            Catch ex As Exception
+                MsgBox(ex.ToString)
+
+                MsgBox("Error")
+                Return
+            End Try
+
+            ButtonConnect.Text = "Disconnect"
+            TextPort.Enabled = False
+            TextIP.Enabled = False
+            TextUsername.Enabled = False
+            TextChat.Enabled = True
+            ButtonSend.Enabled = True
+        Else
+
+            thread.Abort()
+            socket.Close()
+            ButtonConnect.Text = "Connect"
+            TextPort.Enabled = True
+            TextIP.Enabled = True
+            TextUsername.Enabled = True
+            TextChat.Enabled = False
+            ButtonSend.Enabled = False
+        End If
+
+
+
+    End Sub
+
+    Private Sub ButtonSend_Click(sender As Object, e As EventArgs) Handles ButtonSend.Click
+        If Not (TextChat.Text = "") Then
+            Dim bytes As Byte() = System.Text.Encoding.ASCII.GetBytes(TextChat.Text & vbCrLf)
+            Dim BytesEnvoyes As Integer = socket.Send(bytes)
+            TextChat.Text = ""
+        End If
+    End Sub
+
+    Public Sub putMessage(ByVal TextMessages As TextBox, ByVal message As String)
+        messages = messages & message
+        If TextMessages.InvokeRequired Then
+            TextMessages.Invoke(Sub() putMessage(TextMessages, messages))
+        Else
+            TextMessages.Text = messages
+        End If
+
+    End Sub
+
+    Private Sub Receive()
+        While (True)
+            Dim bytes(255) As Byte
+            Dim bytesCount As Integer = socket.Receive(bytes)
+            Dim i As String = System.Text.Encoding.ASCII.GetString(bytes)
+
+            putMessage(TextMessages, System.Text.Encoding.ASCII.GetString(bytes))
+        End While
+    End Sub
+End Class
